d7a0ca35981e9f7e417e3ac18d1e388e852b26cb
Pedro <pedrocasetta@protonmail.com>
2023-10-07 14:51:36 -0300

    commit_node

--- semana7/node/src/Relogio.js
+++ semana7/node/src/Relogio.js
@@ -1,5 +1,6 @@
 import './Relogio.css';
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 function ContaHora(){
     let objeto_data_tempo_real = new Date();
@@ -7,12 +8,15 @@ function ContaHora(){
     let minuto_atual = objeto_data_tempo_real.getMinutes();
     let segundo_atual = objeto_data_tempo_real.getSeconds();
     let tempo_atual = hora_atual + ":" + minuto_atual + ":" + segundo_atual;
-    document.getElementById('root').textContent = tempo_atual;
-    setInterval(ContaHora, 1000);
+    return <h1>{tempo_atual}</h1>
 }
 
-function Hora() {
-    setInterval(ContaHora, 1000);
-}
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-export default ContaHora;
+function Compasso() {
+    root.render(<ContaHora />)
+}
+setInterval(Compasso, 1000);
+
+export default Compasso;
